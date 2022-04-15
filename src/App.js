e1a518b2b763ca662fc681fd087d3b4cb832d1b4
Aline <eualine2017@gmail.com>
2022-04-14 21:17:56 -0300

    feat: delete original css, add contacts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import helloWorld from './images/helloWorld.gif';
 import profilePhoto from './images/profilePhoto.png';
+import contacts from './images/contacts.gif';
 
 function App() {
   return (
@@ -14,6 +15,18 @@ function App() {
         <p>I believe that technology can increasingly change people's lives for the better, both as a rewarding job (which gave my life meaning), and as a solution to problems we face or will face.</p>
         <p>As a developer I have knowledge in the following technologies: JavaScript, HTML, CSS, React.js, Redux, Typescript, Docker, Node.js, Express.js, MySQL, React Testing Library, Mocha, Chai, Jest, Sinon, etc.</p>
       </article>
+      <section id="section-contacts">
+        <img src={contacts} alt="contacts" id="contacts-image"/>
+      </section>
+      <div id="div-links">
+        <p>Email: eualine2017@gmail.com</p>
+        <a href="https://linkedin.com/in/alinecarolina2022" target="blank">Linkedin</a>
+        <a href="https://github.com/AlineCarolina">GitHub</a>
+        <p></p>
+      </div>
+      <footer id="footer-text">
+        <p id="aline-copyright">©Aline Carolina 2022</p>
+      </footer>
     </div>
   );
 }
