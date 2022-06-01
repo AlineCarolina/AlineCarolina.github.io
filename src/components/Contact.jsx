1fc16c6d9aaf79fbf2f455ecdc8123e73d2f2d92
Aline <eualine2017@gmail.com>
2022-06-01 15:07:28 -0300

    feat: margins css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,23 +9,24 @@ import '../css/Contact.css';
 class Contact extends React.Component {
   render() {
     return (
-      <div id="div-contact">
-        <h2>Contact me!</h2>
-        <div id="div-links">
-          <a href="mailto:eualine2017@gmail.com" target='_blank' rel="noreferrer" >
-            <img src={gmail} alt="gmail" id="gmail-image"/>
-          </a>
-          <a href="https://linkedin.com/in/alinecarolina2022" target='_blank' rel="noreferrer">
-            <img src={linkedin} alt="linkedin" id="linkedin-image"/>
-          </a>
-          <a href="https://github.com/AlineCarolina" target='_blank' rel="noreferrer">
-            <img src={github} alt="github" id="github-image"/>
-          </a>
-          <a href="https://api.whatsapp.com/send?phone=5544988342697&text=Olá, vi seu portifólio ..." target='_blank' rel="noreferrer">
-            <img src={whatsapp} alt="whatsaap" id="whatsapp-image"/>
-          </a>
+        <div id="div-contact">
+          <h2>Contact me!</h2>
+          <div id="div-links">
+            <a href="mailto:eualine2017@gmail.com" target='_blank' rel="noreferrer" >
+              <img src={gmail} alt="gmail" id="gmail-image"/>
+            </a>
+            <a href="https://linkedin.com/in/alinecarolina2022" target='_blank' rel="noreferrer">
+              <img src={linkedin} alt="linkedin" id="linkedin-image"/>
+            </a>
+            <a href="https://github.com/AlineCarolina" target='_blank' rel="noreferrer">
+              <img src={github} alt="github" id="github-image"/>
+            </a>
+            <a href="https://api.whatsapp.com/send?phone=5544988342697&text=Olá, vi seu portifólio ..." target='_blank' rel="noreferrer">
+              <img src={whatsapp} alt="whatsaap" id="whatsapp-image"/>
+            </a>
+          </div>
         </div>
-      </div>
+      
     );
   }
 }
