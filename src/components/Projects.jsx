be336650391c55213f45d566b0889f3fb70b9338
Aline <eualine2017@gmail.com>
2022-06-17 10:05:15 -0300

    fix: changing the css of the projects page that interfered with the other pages

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,8 +11,8 @@ class Projects extends React.Component {
           <li>
             <a href='https://sound-on-musics-alinecarolina.vercel.app/' target='_blank' rel='noreferrer' id="img-sound-on">
               <div className='div-img-h3'>
-                <h3>Sound On - Musics</h3>
-                <img src={ImgSound} alt="GIF do Projeto Sound On - Musics" id="img-sound-on" />
+                <h3 className='h3-name-project'>Sound On - Musics</h3>
+                <img src={ImgSound} alt="GIF do Projeto Sound On - Musics" id="img-sound-on"/>
               </div>
               <p>
               React application, using the Itunes API to play songs from the most varied bands and artists, create a list of favorite songs and edit the profile of the user logged in.
@@ -22,8 +22,8 @@ class Projects extends React.Component {
           <li>
             <a href='https://github.com/AlineCarolina/Blogs-API' target='_blank' rel='noreferrer'>
               <div className='div-img-h3'>
-                <h3>Blogs API</h3>
-                <img src="https://picsum.photos/id/1/100/100" alt="Imagem do Projeto 1" />
+                <h3 className='h3-name-project'>Blogs API</h3>
+                <img src="https://picsum.photos/id/1/100/100" alt="Imagem do Projeto 1" className='img-project' />
               </div>
               <p>
               I architected and developed a CRUD API for blog posts (with Sequelize and following REST principles).
@@ -33,8 +33,8 @@ class Projects extends React.Component {
           <li>
             <a href='https://github.com/AlineCarolina/Trybesmith' target='_blank' rel='noreferrer'>
               <div className='div-img-h3'>
-                <h3>Trybesmith</h3>
-                <img src="https://picsum.photos/id/2/100/100" alt="Imagem do Projeto 2" />
+                <h3 className='h3-name-project'>Trybesmith</h3>
+                <img src="https://picsum.photos/id/2/100/100" alt="Imagem do Projeto 2" className='img-project' />
               </div>
               <p>
               For this project, I developed a CRUD (Create, Read, Update and Delete) of medieval items, in an API format, using Typescript and a MySQL database.
@@ -44,8 +44,8 @@ class Projects extends React.Component {
           <li>
             <a href='https://github.com/AlineCarolina/StarWars' target='_blank' rel='noreferrer'>
               <div className='div-img-h3'>
-                <h3>StarWars Search Planets</h3>
-                <img src="https://picsum.photos/id/1/100/100" alt="Imagem do Projeto 3" />
+                <h3 className='h3-name-project'>StarWars Search Planets</h3>
+                <img src="https://picsum.photos/id/1/100/100" alt="Imagem do Projeto 3" className='img-project' />
               </div>
               <p>
               In this project, a list with filters of planets from the Star Wars universe was developed using Context API and Hooks to control global states.
