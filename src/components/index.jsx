644942d3df3f3361c2098c0ffe4b0024118db413
Aline <eualine2017@gmail.com>
2022-09-21 14:37:54 -0300

    fix: deleting contact page from index file

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,4 +3,3 @@ export { default as Routes } from './Routes';
 export { default as Footer } from './Footer';
 export { default as PersonalInfo } from './PersonalInfo';
 export { default as Projects } from './Projects';
-export { default as Contact } from './Contact';
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,3 @@
 export { default as AboutMe } from './AboutMe';
-export { default as ContactPage } from './ContactPage';
 export { default as ProjectsPage } from './ProjectsPage';
 export { default as SkillsPage} from './SkillsPage'
