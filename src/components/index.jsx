28598fdd5e86bac0e59d44ad0970c367f2340d3d
Aline Carolina <eualine2017@gmail.com>
2025-01-03 15:39:32 -0300

    chore: remove old unused files

--- src/components/index.jsx
+++
@@ -1,6 +0,0 @@
-export { default as Nav } from './Nav';
-export { default as Routes } from './Routes';
-export { default as Footer } from './Footer';
-export { default as PersonalInfo } from './PersonalInfo';
-export { default as Projects } from './Projects';
-export { default as Skills } from './Skills';
--- src/pages/index.jsx
+++
@@ -1,3 +0,0 @@
-export { default as AboutMe } from './AboutMe';
-export { default as ProjectsPage } from './ProjectsPage';
-export { default as SkillsPage} from './SkillsPage'
