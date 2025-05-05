ba9929fce802ef3ccc56aeda2f3cc6d3c8efc660
Aline Carolina <eualine2017@gmail.com>
2025-05-05 20:01:59 -0300

    feat: add gh-pages for deployment and update package.json; modify vite.config.js base path

--- meuportifolio/vite.config.js
+++ meuportifolio/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "AlineCarolina.github.io",
   plugins: [react()],
   server: {
     watch: {
