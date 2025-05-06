f4bf720d1f41c1145b06ff7346a72b020e5f467c
Aline Carolina <eualine2017@gmail.com>
2025-05-06 14:45:08 -0300

    Refactor: Update script source in index.html, remove react-router-dom dependency, and simplify App component structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { BrowserRouter as Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Main from './pages/Main';
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Main />
       <Footer />
-    </Router>
+    </>
   );
 }
 
