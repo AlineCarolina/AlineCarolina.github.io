c7a3c970d238f076f645286c98aa771d8da323ff
Aline Carolina <eualine2017@gmail.com>
2025-05-06 10:35:45 -0300

    add dist

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "AlineCarolina.github.io",
+  base: "alinecarolina.github.io",
   plugins: [react()],
   server: {
     watch: {
